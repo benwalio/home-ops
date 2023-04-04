d1114c28290a40a0ea82939238cdc6b8c7295bb8
ben <ben.wallace@protonmail.ch>
2023-04-04 15:52:50 -0400

    fix(misc): toggle

--- terraform/cloudflare/domain_casa.tf
+++ terraform/cloudflare/domain_casa.tf
@@ -1,7 +1,7 @@
 
 data "cloudflare_zones" "domain" {
   filter {
-    name = data.sops_file.cloudflare_secrets.data["cloudflare_domain_casa"]
+    name = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
   }
 }
 
@@ -58,10 +58,125 @@ resource "cloudflare_record" "ipv4" {
 }
 
 resource "cloudflare_record" "root" {
-  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain_casa"]
+  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain_casa"]}"
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
   type    = "CNAME"
   ttl     = 1
+}
+
+
+resource "cloudflare_record" "email_domain_simplelogin_cname1" {
+  name    = "dkim._domainkey"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "dkim._domainkey.simplelogin.co"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "email_domain_simplelogin_cname2" {
+  name    = "dkim02._domainkey"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "dkim02._domainkey.simplelogin.co"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "email_domain_simplelogin_cname3" {
+  name    = "dkim03._domainkey"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "dkim03._domainkey.simplelogin.co"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
+
+
+resource "cloudflare_record" "email_domain_simplelogin_mx1" {
+  name     = "benwal.casa"
+  zone_id  = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value    = "mx1.simplelogin.co"
+  proxied  = false
+  type     = "MX"
+  ttl      = 1
+  priority = 10
+}
+
+resource "cloudflare_record" "email_domain_simplelogin_mx2" {
+  name     = "benwal.casa"
+  zone_id  = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value    = "mx2.simplelogin.co"
+  proxied  = false
+  type     = "MX"
+  ttl      = 1
+  priority = 20
+}
+
+resource "cloudflare_record" "email_domain_simplelogin_spf" {
+  name    = "benwal.casa"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "v=spf1 include:simplelogin.co ~all"
+  proxied = false
+  type    = "TXT"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "email_domain_simplelogin_verify" {
+  name    = "benwal.casa"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "sl-verification=ckrkbwmpugmrolvmrsqkdycqqjhwye"
+  proxied = false
+  type    = "TXT"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "email_domain_simplelogin_dmarc" {
+  name    = "_dmarc"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s"
+  proxied = false
+  type    = "TXT"
+  ttl     = 1
+}
+
+resource "cloudflare_page_rule" "domain_plex_bypass" {
+  zone_id  = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  target   = "https://plex.benwal.casa./*"
+  status   = "active"
+  priority = 1
+
+  actions {
+    cache_level              = "bypass"
+    rocket_loader            = "off"
+    automatic_https_rewrites = "on"
+  }
+}
+
+resource "cloudflare_page_rule" "domain_home_assistant_bypass" {
+  zone_id  = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  target   = "https://hass.benwal.casa./*"
+  status   = "active"
+  priority = 2
+
+  actions {
+    cache_level              = "bypass"
+    rocket_loader            = "off"
+    automatic_https_rewrites = "on"
+  }
+}
+
+resource "cloudflare_page_rule" "domain_photos_bypass" {
+  zone_id  = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  target   = "https://photos.benwal.casa./*"
+  status   = "active"
+  priority = 3
+
+  actions {
+    cache_level              = "bypass"
+    rocket_loader            = "off"
+    automatic_https_rewrites = "on"
+  }
 }
